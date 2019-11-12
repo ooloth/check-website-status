442603ab30606334d311c668a5cb1abc8444f580
Michael Uloth <hello@michaeluloth.com>
2019-11-11 22:20:22 -0500

    🧠 add the logic that checks each URL and logs the results

---
+++ index.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+const { mySites, myCoffeeshopSites } = require("./sites");
+
+async function checkUrl(url) {
+  try {
+    await fetch(url);
+    return null;
+  } catch (error) {
+    console.log(`😱  ${error.code}: ${url}`);
+    return url;
+  }
+}
+
+async function checkUrls() {
+  const brokenLinks = await Promise.all(
+    [...mySites, ...myCoffeeshopSites].map(async url => await checkUrl(url))
+  );
+
+  if (brokenLinks.filter(Boolean).length > 1) {
+    return null;
+  }
+
+  console.log(`😎  All websites are online.`);
+}
+
+checkUrls();
