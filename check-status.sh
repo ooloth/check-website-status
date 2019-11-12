2ec62e587c8e097e540942492abb80ccdaa8181e
Michael Uloth <hello@michaeluloth.com>
2019-11-11 22:20:58 -0500

    🧠 add a shell script that runs the node script via a cron job

---
+++ check-status.sh
@@ -0,0 +1,6 @@
+# Cron job script
+
+#!/usr/local/bin/zsh sh
+PATH=/Users/Michael/.nvm/versions/node/v12.6.0/bin:/Users/Michael/.rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+node index.js
